763c3ad033f88e6d9304a37d312da01ce2926939
jairo <jairo@169.254.144.173>
2015-03-12 08:40:34 -0500

    Merge origin/master
    Conflicts:
    	public_html/index.html
    	public_html/src/app.js
    	public_html/src/modules/Hotel/hotel.ctrl.js
    	public_html/src/modules/Hotel/hotel.tpl.html
    	public_html/src/shared/crud/crud.svc.js

--- public_html/src/modules/Itinerary/itinerary.ctrl.js
+++ public_html/src/modules/Itinerary/itinerary.ctrl.js
@@ -1,11 +1,38 @@
 (function () {
 	var app = angular.module('itineraryModule');
 
-	app.controller('itineraryCtrl', ['$scope', 'CRUDUtils', 'itinerary.context', function ($scope, CRUDUtils, context) {
+	app.controller('itineraryCtrl', ['$scope', 'CRUDUtils','ciudad.context', 'itineraryService','eventService', function ($scope, CRUDUtils, context,eventService,ciudadService) {
 			this.url = context;
 			CRUDUtils.extendCtrl(this, $scope);
                       
 			this.fetchRecords();
+                        ciudadService.fetchRecords().then(function(data){
+                            $scope.ciudadRecords = data;
+                            });
+                            
+                        this.fetchRecords();
+                        eventService.fetchRecords().then(function(data){
+                            $scope.eventRecords = data;
+                            });    
+                            
+                             this.getCiudadName = function(id){
+                for (var i in $scope.ciudadRecords) {
+                    
+                    if ($scope.ciudadRecords[i].id === id) {
+                        return $scope.ciudadRecords[i].name;
+                    }
+                }
+                return;
+                    };
+            this.getEnventName= function(id){
+                for (var i in $scope.eventRecords) {
+                    if ($scope.eventRecords[i].id === id) {
+                        return $scope.eventRecords[i].name;
+                    }
+                }
+                return;
+            };
+              
 		}]);                                  
             
             
