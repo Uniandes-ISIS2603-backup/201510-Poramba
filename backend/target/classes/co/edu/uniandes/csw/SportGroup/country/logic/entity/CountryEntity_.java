48b7defe992af0e79c75cc267f30befb3e00e68d
Jairo Bernal <ji.bernal27@uniandes.edu.co>
2015-03-18 17:32:26 -0500

    Angular

---
+++ backend/target/classes/co/edu/uniandes/csw/SportGroup/country/logic/entity/CountryEntity_.java
@@ -0,0 +1,15 @@
+package co.edu.uniandes.csw.SportGroup.country.logic.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2015-03-18T16:49:15")
+@StaticMetamodel(CountryEntity.class)
+public class CountryEntity_ { 
+
+    public static volatile SingularAttribute<CountryEntity, Long> id;
+    public static volatile SingularAttribute<CountryEntity, String> name;
+    public static volatile SingularAttribute<CountryEntity, Integer> population;
+
+}
