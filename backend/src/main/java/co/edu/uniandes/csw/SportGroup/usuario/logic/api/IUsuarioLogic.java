18dd3412318138dc7720945e9790b9c59b37a3c6
Luis Angel Cervantes Cortes <la.cervantes10@uniandes.edu.co>
2015-03-19 00:51:02 -0500

    agregacion del api usuario

---
+++ backend/src/main/java/co/edu/uniandes/csw/SportGroup/usuario/logic/api/IUsuarioLogic.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.SportGroup.usuario.logic.api;
+
+import co.edu.uniandes.csw.SportGroup.country.logic.dto.CountryDTO;
+import co.edu.uniandes.csw.SportGroup.usuario.logic.dto.UsuarioDTO;
+import co.edu.uniandes.csw.SportGroup.usuario.logic.dto.UsuarioPageDTO;
+import java.util.List;
+
+/**
+ *
+ * @author afesguerra
+ */
+public interface IUsuarioLogic {
+
+    public CountryDTO createUsuario(UsuarioDTO detail);
+
+    public List<UsuarioDTO> getUsuarios();
+
+    public UsuarioPageDTO getUsuarios(Integer page, Integer maxRecords);
+
+    public CountryDTO getUsuario(Long id);
+
+    public void deleteUsuario(Long id);
+
+    public void updateUsuario(UsuarioDTO detail);
+    
+    /*public UsuarioDTO getMostPopulated();
+    
+    public UsuarioDTO getLeastPopulated();*/
+}
