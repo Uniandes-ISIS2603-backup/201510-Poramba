adb20677f1ce9c5b3019d3fff846c6ecf4a711c4
Jairo Bernal <ji.bernal27@uniandes.edu.co>
2015-03-25 15:27:52 -0500

    Merge branch 'master' of https://github.com/Uniandes-ISIS2603/201510-Poramba
    Conflicts:
    	backend/src/main/java/co/edu/uniandes/csw/LugarGroup/lugar/logic/ejb/LugarLogic.java

--- backend/src/main/java/co/edu/uniandes/csw/LugarGroup/lugar/logic/ejb/LugarLogic.java
+++ backend/src/main/java/co/edu/uniandes/csw/LugarGroup/lugar/logic/ejb/LugarLogic.java
@@ -13,28 +13,4 @@ public class LugarLogic implements ILugarLogic{
     @PersistenceContext(unitName = "CiudadClassPU")
     protected EntityManager entityManager;
 
-    public CiudadDTO createCiudad(CiudadDTO ciudad) {
-        CiudadEntity entity = CiudadConverter.persistenceDTO2Entity(ciudad);
-        entityManager.persist(entity);
-        return CiudadConverter.entity2PersistenceDTO(entity);
-    }
-
-    public List<CiudadDTO> getCiudades() {
-        Query q = entityManager.createQuery("select u from CiudadEntity u");
-        return CiudadConverter.entity2PersistenceDTOList(q.getResultList());
-    }
-
-    public CiudadDTO getCiudad(Long id) {
-        return CiudadConverter.entity2PersistenceDTO(entityManager.find(CiudadEntity.class, id));
-    }
-
-    public void deleteCiudad(Long id) {
-        CiudadEntity entity = entityManager.find(CiudadEntity.class, id);
-        entityManager.remove(entity);
-    }
-
-    public void updateCiudad(CiudadDTO ciudad) {
-        CiudadEntity entity = entityManager.merge(CiudadConverter.persistenceDTO2Entity(ciudad));
-        CiudadConverter.entity2PersistenceDTO(entity);
-    }
 }
