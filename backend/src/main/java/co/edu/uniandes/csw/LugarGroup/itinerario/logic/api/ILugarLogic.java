d7acbe8c733e774e75738f0f9a5aa5ace3cbcb00
Jairo Bernal <ji.bernal27@uniandes.edu.co>
2015-03-25 15:28:09 -0500

    Merge branch 'master' of https://github.com/Uniandes-ISIS2603/201510-Poramba
    Conflicts:
    	backend/src/main/java/co/edu/uniandes/csw/LugarGroup/lugar/logic/ejb/LugarLogic.java

--- backend/src/main/java/co/edu/uniandes/csw/LugarGroup/itinerario/logic/api/ILugarLogic.java
+++ backend/src/main/java/co/edu/uniandes/csw/LugarGroup/itinerario/logic/api/ILugarLogic.java
@@ -19,14 +19,30 @@ public interface ILugarLogic {
 
     public LugarDTO createLugar(LugarDTO detail);
 
-    public List<CiudadDTO> getCiudades();
     /**
-     * Agrega una cidad al lugar
+     * Obtiene la ciudad del lugar actual
+     * @return  Ciudad del luga actual, nulo si no existe
+     */
+    public CiudadDTO getCiudad();
+    /**
+     * Agrega una cidad a el  lugar
      * @param detail Parametros de la ciudad a agregar
-     * @return Retona la ciudad 
+     * @return Retorna la ciudad, nulo si ya eiste una ciudad asociada 
      */
     public CiudadDTO addCiudad(CiudadDTO detail);
+   
+    /**
+     * Elimina la ciudad actual del lugar
+     * @return  true si elimina, false en caso contrario
+     */
+    public boolean deleteCiudad();
     
+    /**
+     * Reemplaza la ciudad actual por una dada por parametro
+     * @param ciudad La ciudad por la cual se va a cambiar la actual
+     * @return La nueva ciuad agregada.
+     */
+    public CiudadDTO updateCiudad(CiudadDTO ciudad);
     /**
      * Agregar un evento al lugar
      * @param detail Evento a agregar
@@ -34,14 +50,63 @@ public interface ILugarLogic {
      */
     public EventoDTO addEvento(EventoDTO detail);
     
+    /**
+     * Retorna un evento asociado al itinerario con el id dado por parametro
+     * @param id Id del evento a buscar
+     * @return El evento que tiene la id dada por paramentro, nulo si no la encuntra.
+     */
+    public EventoDTO getEvento(String id);
     
+    /**
+     * Retorna todos los eventos 
+     * @return ista con los eventos del lugar
+     */
+    public List<EventoDTO> getEventos();
+    
+    /**
+     * Elimina un evento del lugar asociado al id dado por parametro
+     * @param id Id del evento a eliminar
+     * @return true si elimina, false en caso contrario
+     */
+    public boolean deleteEvento(String id);
+    
+    /**
+     * Elimina todos los eventos, hotel y tranporte asociados al lugar.
+     * @return true si elimina todo, false en caso contrario. 
+     */
+    public boolean cleanLugar();
+    
+    /**
+     * Cambia el evento por la id por parametro por el nuevo Evento pasado por parametro
+     * @param idActual Id del evento a reemplazar.                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+     * @param nuevo Nuevo Evento a agregar.
+     * @return El nuevo evento agregado, null si no pudo agregarlo
+     */
+    public EventoDTO updateEvento(String idActual,EventoDTO nuevo );
     /**
      * El transporte que se va a agregar al lugar
      * @param detail Transporte a agregar
      * @return Retorna el nuevo transporte agregado
      */
     public transporteDTO addTransporte(transporteDTO detail);
-
+    
+    /**
+     * Reemplaza el tranporte actual por uno dado por parametro
+     * @param detail Nuevo transporte
+     */
+    public void updateTransporte(transporteDTO detail);
+    
+    /**
+     * Elimian el transporte actual
+     * @return true si se elimina el transporte, false en caso contrario
+     */
+    public boolean deleteTRanporte();
+    
+    /**
+     * Retorna el transporte actual del lugar 
+     * @return El transporte actual
+     */
+    public transporteDTO getTransporte();
     /**
      * El nuevo hotel a agregar en el lugar.
      * @param detail Nuevo hotel
@@ -49,18 +114,36 @@ public interface ILugarLogic {
      */
     
     public HotelDTO addHotel(HotelDTO detail);
+   /**
+    * Elimian el hotel actual
+    * @return true si se elimina, false en caso contrario. 
+    */
+    public boolean deleteHotel();
+    
+    /**
+     * Retorna el hotel actual.
+     * @return EL hotel actual, null si no esxite uno
+     */
+    public HotelDTO getHotel();
+    
+    /**
+     * Cambia el hotelactual por uno dado por parametro
+     * @param hotel El nuevo hotel
+     * @return 
+     */
+    public HotelDTO setHotel(HotelDTO hotel);
     /**
      * Obtener un lugar a partir de su id.
      * @param id Id del lugar, esta es unica
      * @return Lugar Buscado
      */
-    public LugarDTO getLugar(Long id);
+    public LugarDTO getLugar(String id);
 
     /**
      * Elimina el lugar asociado a la id
      * @param id Id del lugar a eliminar
      */
-    public void deleteLugar(Long id);
+    public void deleteLugar(String id);
     
     /**
      * Cambia el lugar actual por otro
