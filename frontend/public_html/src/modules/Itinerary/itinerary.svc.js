6c6da70f424bab14d8e7f17ffe6680a76ea143ad
a-cordoba10 <Icoltus@Alejandros-Mac-Pro.local>
2015-03-26 07:45:16 -0500

    2

--- frontend/public_html/src/modules/Itinerary/itinerary.svc.js
+++ frontend/public_html/src/modules/Itinerary/itinerary.svc.js
@@ -1,6 +1,6 @@
 (function () {
     var itineraryModule = angular.module('itineraryModule');
-    eventModule.service('itineraryService', ['CRUDBase', 'itinerary.context', function (CRUDBase, context) {
+    itineraryModule.service('itineraryService', ['CRUDBase', 'itinerary.context', function (CRUDBase, context) {
             this.url = context;
             CRUDBase.extendService(this);
         }]);
