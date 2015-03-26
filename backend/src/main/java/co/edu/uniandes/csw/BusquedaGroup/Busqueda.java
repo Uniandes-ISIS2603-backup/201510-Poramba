a0744f2b60a88be83c89995401f6d6446e9415f2
aiMojica10 <ai.mojica10@uniandes.edu.co>
2015-03-25 22:41:09 -0500

    Merge origin/master

--- backend/src/main/java/co/edu/uniandes/csw/BusquedaGroup/Busqueda.java
+++ backend/src/main/java/co/edu/uniandes/csw/BusquedaGroup/Busqueda.java
@@ -2,12 +2,37 @@
  * Paquete de clase busqueda
  */
 package co.edu.uniandes.csw.BusquedaGroup;
-
+import  org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Iterator;
 /**
  *Clase en la que se realiza la utilizacion del appi 
  * para busuqedas
  */
 public class Busqueda 
 {
-    
+    private JSONObject darJSON(String direc) throws IOException {
+		URL url = new URL(direc);
+		HttpURLConnection request = (HttpURLConnection) url.openConnection();
+		request.connect();
+		JSONParser parser = new JSONParser();
+		Object root;
+		try {
+			root = parser.parse(new InputStreamReader((InputStream) request
+					.getContent()));
+			JSONObject jsonObject = (JSONObject) root;
+			return jsonObject;
+		}
+
+		catch (org.json.simple.parser.ParseException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
