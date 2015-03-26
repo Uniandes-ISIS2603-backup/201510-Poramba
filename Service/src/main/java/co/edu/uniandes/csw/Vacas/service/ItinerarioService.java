ec3062070a4aeb5480505a9277a1d4f3adee7796
Jairo Bernal <ji.bernal27@uniandes.edu.co>
2015-03-26 09:53:14 -0500

    .Servie

---
+++ Service/src/main/java/co/edu/uniandes/csw/Vacas/service/ItinerarioService.java
@@ -0,0 +1,74 @@
+package co.edu.uniandes.csw.Vacas.service;
+
+
+
+import co.edu.uniandes.csw.CiudadGroup.ciudad.logic.dto.CiudadDTO;
+import co.edu.uniandes.csw.LugarGroup.lugar.logic.dto.LugarDTO;
+import co.edu.uniandes.csw.itinerarioGroup.itinerario.logic.api.IitinerarioLogic;
+import co.edu.uniandes.csw.itinerarioGroup.itinerario.logic.dto.itinerarioDTO;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author estudiante
+ */
+@Path("/intinerary")
+@Stateless
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class ItinerarioService 
+{
+
+    @Inject
+    protected IitinerarioLogic countryLogic;
+
+    @POST
+    public itinerarioDTO createItinerario(itinerarioDTO itinerario) {
+        return countryLogic.createItinerario(itinerario);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void deleteItinerario(@PathParam("id") String id) {
+        countryLogic.deleteItinerario(id);
+    }
+
+    @GET
+    public List<itinerarioDTO> getItinerarios() {
+        return countryLogic.getItinerarios();
+    }
+
+    @GET
+    @Path("{id}")
+    public itinerarioDTO getItinerario(@PathParam("id") String id) {
+        return countryLogic.getItinerario(id);
+    }
+
+    @PUT
+    public void updateItinerario( itinerarioDTO itinerario) {
+        countryLogic.updateItinerario(itinerario);
+    }
+    
+    @PUT
+    @Path("{agregarCiudad}")
+    public LugarDTO updateItinerario( LugarDTO ciudad) 
+    {
+        return countryLogic.addLugar(ciudad);
+    }
+    
+    
+
+   
+}
---
+++ Service/src/main/java/co/edu/uniandes/csw/Vacas/service/LugarService.java
@@ -0,0 +1,62 @@
+package co.edu.uniandes.csw.Vacas.service;
+
+
+import co.edu.uniandes.csw.CiudadGroup.ciudad.logic.api.ICiudadLogic;
+import co.edu.uniandes.csw.CiudadGroup.ciudad.logic.dto.CiudadDTO;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author estudiante
+ */
+@Path("/city")
+@Stateless
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class LugarService 
+{
+
+    @Inject
+    protected ICiudadLogic countryLogic;
+
+    @POST
+    public CiudadDTO createCiudad(CiudadDTO ciudad) {
+        return countryLogic.createCiudad(ciudad);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void deleteCiudad(@PathParam("id") String id) {
+        countryLogic.deleteCiudad(id);
+    }
+
+    @GET
+    public List<CiudadDTO> getCountries() {
+        return countryLogic.getCiudades();
+    }
+
+    @GET
+    @Path("{id}")
+    public CiudadDTO getCiudad(@PathParam("id") String id) {
+        return countryLogic.getCiudad(id);
+    }
+
+    @PUT
+    public void updateCiudad( CiudadDTO ciudad,@PathParam("id") String id) {
+        countryLogic.updateCiudad(ciudad,id);
+    }
+
+   
+}
