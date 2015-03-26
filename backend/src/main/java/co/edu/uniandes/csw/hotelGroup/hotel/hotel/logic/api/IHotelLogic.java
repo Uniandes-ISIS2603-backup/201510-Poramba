07246ec0a08e4be423e2cf147582ff15fe841c0b
estudiante <estudiante@172.24.98.162>
2015-03-25 21:03:34 -0500

    Merge origin/master
    Conflicts:
    	backend/target/classes/co/edu/uniandes/csw/CiudadGroup/ciudad/logic/api/ICiudadLogic.class
    	backend/target/classes/co/edu/uniandes/csw/CiudadGroup/ciudad/logic/dto/CiudadDTO.class
    	backend/target/classes/co/edu/uniandes/csw/CiudadGroup/ciudad/logic/dto/CiudadPageDTO.class
    	backend/target/classes/co/edu/uniandes/csw/CiudadGroup/ciudad/logic/entity/CiudadEntity.class
    	backend/target/classes/co/edu/uniandes/csw/SportGroup/evento/logic/dto/EventoDTO.class
    	backend/target/classes/co/edu/uniandes/csw/eventoGroup/evento/logic/api/IEventoLogic.class
    	backend/target/classes/co/edu/uniandes/csw/eventoGroup/evento/logic/converter/EventoConverter.class

--- backend/src/main/java/co/edu/uniandes/csw/hotelGroup/hotel/hotel/logic/api/IHotelLogic.java
+++ backend/src/main/java/co/edu/uniandes/csw/hotelGroup/hotel/hotel/logic/api/IHotelLogic.java
@@ -15,14 +15,34 @@ import java.util.List;
  */
 public interface IHotelLogic {
 
+    /**
+     * Crea un nuevo hotel y lo agrega a la base de datos.
+     * @param detail hotel nuevo.
+     * @return El nuevo hotel creado
+     */
     public HotelDTO createHotel(HotelDTO detail);
 
+    /**
+     * 
+     * @return Retorna la lista de hoteles.
+     */
     public List<HotelDTO> getHoteles();
+    /**
+     * Da el hotel con el identificador que entra por parametro
+     * @param id String con el identificador nuevo.
+     * @return Hotel con el identificador que etra por parametro
+     */
+    public HotelDTO getHotel(String id);
+    /**
+     *  Elimina de la base de datos un hotel con el identificador que entra por parametro
+     * @param id String con identificador que entra por parametro
+     */
+    public void deleteHotel(String id);
 
-    public HotelDTO getHotel(Long id);
-
-    public void deleteHotel(Long id);
-
+    /**
+     * Actualiza un hotel con la información que entra por parametro
+     * @param detail información del hotel
+     */
     public void updateHotel(HotelDTO detail);
     
 }
