f8a40cb13cafc0f890e5375e57420ef7553ba0b0
Jairo Bernal <ji.bernal27@uniandes.edu.co>
2015-05-05 11:15:27 -0500

    Revert "JairoBOBOn"
    This reverts commit 68e325df7690f76b6691adbdc05824fb5d480e90.

--- backend/VacasPoramaba.Service/src/main/webapp/src/modules/Hotel/hotel.ctrl.js
+++ backend/VacasPoramaba.Service/src/main/webapp/src/modules/Hotel/hotel.ctrl.js
@@ -7,7 +7,12 @@
                         
 			this.fetchRecords();
                         
-                       
+                        
+                        this.ana = function()
+                        {
+                           // <---- $location.path('/event').replace();-->
+                            alert ('anama');
+                        };
                          this.inicializarNombre = function(){
                                 $scope.currentRecord.name = 'Pestana casino park';
                             };
@@ -15,7 +20,7 @@
                                 $scope.currentRecord.ubicacionHot = 'Isla de madeira';
                             };
                               this.inicializarDescripcion = function(){
-                                $scope.currentRecord.descripcion = 'EL PESTANA CASINO PARK FORMA PARTE DEL COMPLEJO TUR√?STICO M√?S EMBLEM√?TICO DE LA ISLA DE MADEIRA.';
+                                $scope.currentRecord.descripcion = 'EL PESTANA CASINO PARK FORMA PARTE DEL COMPLEJO TUR√çSTICO M√ÅS EMBLEM√ÅTICO DE LA ISLA DE MADEIRA.';
                             };
                               this.inicializarUrl = function(){
                                 $scope.currentRecord.link = 'http://www.pestana.com/es/pestana-casino-park-hotel/pages/home.aspx';
