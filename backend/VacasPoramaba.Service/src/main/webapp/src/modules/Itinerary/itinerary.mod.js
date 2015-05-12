01375e5c385bf790ed313f4dd1737889e9fadc71
Anamaria  Mojica <ai.mojica10@uniandes.edu.co>
2015-05-12 07:53:04 -0500

    mods

--- backend/VacasPoramaba.Service/src/main/webapp/src/modules/Itinerary/itinerary.mod.js
+++ backend/VacasPoramaba.Service/src/main/webapp/src/modules/Itinerary/itinerary.mod.js
@@ -1,12 +1,9 @@
 (function (angular) {
     var itineraryModule = angular.module('itineraryModule', ['CrudModule','itineraryModule']);
 
-    itineraryModule.constant('itinerary.context', 'itineraries');
-    
-    itineraryModule.constant('itinerary.skipMock', false);
-
-    
-    itineraryModule.config(['itinerary.context', 'MockModule.urlsProvider', function (context, urlsProvider,skipMock) {
+    itineraryModule.constant('itinerary.context', 'itineraries');    
+    itineraryModule.constant('itinerary.skipMock', false);    
+    itineraryModule.config(['itinerary.context', 'MockModule.urlsProvider','itinerary.skipMock', function (context, urlsProvider,skipMock) {
             urlsProvider.registerUrl(context,skipMock);
         }]);
 })(window.angular);
