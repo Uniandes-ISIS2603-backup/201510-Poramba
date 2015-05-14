0bd167c772c983f4635fe21a02b3c4d6c95b6a09
Anamari Mojica <ai.mojica10@uniandes.edu.co>
2015-05-14 00:24:00 -0500

    Muestra, hay errores pero ya  porlmn muestra

--- backend/VacasPoramaba.Service/src/main/webapp/src/modules/traveler/traveler.mod.js
+++ backend/VacasPoramaba.Service/src/main/webapp/src/modules/traveler/traveler.mod.js
@@ -2,7 +2,7 @@
     var travelerModule = angular.module('travelerModule', ['CrudModule', 'MockModule']);
     travelerModule.constant('traveler.context', 'travelers');
     travelerModule.constant('traveler.skipMock', false);
-    travelerModule.config(['traveler.context', 'MockModule.urlsProvider'.'traveler.skipMock', function (context, urlsProvider,skipMock) {
-            urlsProvider.registerUrl(context.skipMock);
+    travelerModule.config(['traveler.context', 'MockModule.urlsProvider','traveler.skipMock', function (context, urlsProvider,skipMock) {
+            urlsProvider.registerUrl(context,skipMock);
         }]);
 })();
