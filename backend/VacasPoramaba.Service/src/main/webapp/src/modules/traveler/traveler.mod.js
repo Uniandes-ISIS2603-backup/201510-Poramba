0eeb46cb021e0c5412e7671de30d18975f49d205
USUARIO <USUARIO@ASUS>
2015-05-14 00:20:31 -0500

    Muestra, hay errores pero ya  porlmn muestra

--- backend/VacasPoramaba.Service/src/main/webapp/src/modules/traveler/traveler.mod.js
+++ backend/VacasPoramaba.Service/src/main/webapp/src/modules/traveler/traveler.mod.js
@@ -1,8 +1,8 @@
-(function () {
+(function (angular) {
     var travelerModule = angular.module('travelerModule', ['CrudModule', 'MockModule']);
     travelerModule.constant('traveler.context', 'travelers');
     travelerModule.constant('traveler.skipMock', false);
-    travelerModule.config(['traveler.context', 'MockModule.urlsProvider'.'traveler.skipMock', function (context, urlsProvider,skipMock) {
-            urlsProvider.registerUrl(context.skipMock);
+    travelerModule.config(['traveler.context', 'MockModule.urlsProvider','traveler.skipMock', function (context, urlsProvider,skipMock) {
+            urlsProvider.registerUrl(context,skipMock);
         }]);
-})();
+})(window.angular);
