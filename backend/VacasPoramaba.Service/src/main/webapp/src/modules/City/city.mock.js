32d9226c082f83b8793bc007c57c052f5681f46a
Anamari Mojica <ai.mojica10@uniandes.edu.co>
2015-05-14 07:38:52 -0500

    city template

--- backend/VacasPoramaba.Service/src/main/webapp/src/modules/City/city.mock.js
+++ backend/VacasPoramaba.Service/src/main/webapp/src/modules/City/city.mock.js
@@ -3,7 +3,7 @@
 
     countryModule.run(['$httpBackend', 'city.context', 'MockModule.mockRecords', 'city.skipMock', 'MockModule.baseUrl', function ($httpBackend, context, mockRecords, skipMock, baseUrl) {
             if (skipMock) {
-//                $httpBackend.whenGET(baseUrl + '/' + context + '/city/conectado').passThrough();
+                $httpBackend.whenGET(baseUrl + '/' + context + '/city/conectado').passThrough();
 //                $httpBackend.whenGET(baseUrl + '/' + context + '/lea').passThrough();
             } else {
             }
