271400aac53f438ab4f961ce49d3079ea97e2962
USUARIO <USUARIO@ASUS>
2015-05-14 07:54:06 -0500

    borrar comentario problematicos sonar

--- backend/VacasPoramaba.Service/src/main/webapp/src/modules/City/city.mock.js
+++ backend/VacasPoramaba.Service/src/main/webapp/src/modules/City/city.mock.js
@@ -4,7 +4,6 @@
     countryModule.run(['$httpBackend', 'city.context', 'MockModule.mockRecords', 'city.skipMock', 'MockModule.baseUrl', function ($httpBackend, context, mockRecords, skipMock, baseUrl) {
             if (skipMock) {
                 $httpBackend.whenGET(baseUrl + '/' + context + '/city/conectado').passThrough();
-//                $httpBackend.whenGET(baseUrl + '/' + context + '/lea').passThrough();
             } else {
             }
         }]);
